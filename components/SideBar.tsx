d9c569f0580f720cb94309bdc9a531c9c2e3900f
Kanif <kumbharkanif666@gmail.com>
2025-03-08 13:01:21 +0530

    Added Left Sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 
 const SideBar = ({ user }: SiderbarProps) => {
+  const pathName = usePathname();
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
           <Image
             src="/icons/logo.png"
             alt="Prysm Logo"
@@ -15,7 +21,34 @@ const SideBar = ({ user }: SiderbarProps) => {
           />
           <h1 className="sidebar-logo">Prysm</h1>
         </Link>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathName === item.route || pathName.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+        USER
       </nav>
+      FOOTER
     </section>
   );
 };
