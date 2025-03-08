44e0cf1e1c562f58a58f99aa6de5017431b29d4c
Kanif <kumbharkanif666@gmail.com>
2025-03-08 17:32:28 +0530

    Added Rigth Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,33 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: "Kanif" };
+  const loggedIn = {
+    firstName: "Kanif",
+    lastName: "Kumbhar",
+    email: "theodddev44@gmail.com",
+  };
 
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || "Guest"}
-          subtext="Access & manage your account & transaction effieciently"
-        />
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1200.45}
-        />
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access & manage your account & transaction effieciently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={120000.45}
+          />
+        </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   );
 };
